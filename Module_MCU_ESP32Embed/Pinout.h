78c962d68612f76d61c5c08e11e3e4593b587302
Luis Llamas <luisllamas@hotmail.com>
2021-05-20 16:51:30 +0200

    Add files via upload

---
+++ Module_MCU_ESP32Embed/Pinout.h
@@ -0,0 +1,75 @@
+#pragma once
+
+static const uint8_t UART_TX = 1;
+static const uint8_t UART_RX = 3;
+
+static const uint8_t IR1 = 2;
+static const uint8_t TOUCH2 = 2;
+
+static const uint8_t SERVO6 = 5;
+
+static const uint8_t TOUCH5 = 12;
+
+static const uint8_t MOTOR1_A = 23;
+static const uint8_t STEPPER1_STEP = 23;
+
+static const uint8_t MOTOR1_B = 19;
+static const uint8_t STEPPER1_DIR = 19;
+
+static const uint8_t MOTOR1_PWM = 27;
+static const uint8_t STEPPER2_STEP = 27;
+
+static const uint8_t MOTOR2_A = 18;
+static const uint8_t STEPPER2_DIR = 18;
+
+static const uint8_t MOTOR2_B = 4;
+static const uint8_t STEPPER3_STEP = 4;
+
+static const uint8_t MOTOR2_PWM = 13;
+static const uint8_t STEPPER3_DIR = 13;
+
+static const uint8_t ENCODER1 = 32;
+static const uint8_t STEPPER4_STEP = 32;
+
+static const uint8_t ENCODER2 = 33;
+static const uint8_t STEPPER4_DIR = 33;
+
+
+static const uint8_t SERVO3 = 14;
+static const uint8_t TOUCH7 = 14;
+
+static const uint8_t IR_2 = 15;
+static const uint8_t TOUCH3 = 15;
+
+static const uint8_t AUX3 = 16;
+static const uint8_t SERVO5 = 16;
+static const uint8_t SONAR3 = 16;
+static const uint8_t UART2_TX = 16;
+
+static const uint8_t SONAR2 = 17;
+static const uint8_t SERVO4 = 17;
+static const uint8_t UART2_RX = 17;
+
+
+static const uint8_t I2C_SDA = 21;
+static const uint8_t I2C_SCL = 22;
+
+static const uint8_t AUX1 = 25;
+static const uint8_t DAC_1 = 25;
+static const uint8_t SERVO1 = 25;
+static const uint8_t SONAR1 = 25;
+
+static const uint8_t AUX2 = 26;
+static const uint8_t DAC_2 = 26;
+static const uint8_t SERVO2 = 26;
+
+static const uint8_t ANALOG3 = 34;
+static const uint8_t IR3 = 34;
+
+static const uint8_t BAT_SENSE = 35;
+
+static const uint8_t ANALOG1 = 36;
+static const uint8_t IR4 = 36;
+
+static const uint8_t ANALOG2 = 39;
+static const uint8_t IR5 = 39;
